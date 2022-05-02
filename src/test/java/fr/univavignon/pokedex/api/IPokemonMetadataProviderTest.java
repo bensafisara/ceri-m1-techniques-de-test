b3704c60eb8a5b2c5efd23fd4723abc9479c21fe
bensafisara <benfisara2@gmail.com>
2022-05-02 14:10:32 +0200

    commit tP3 non terminé 1 !

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,30 +1,53 @@
 package fr.univavignon.pokedex.api;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import junit.framework.TestCase;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.*;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
-public class IPokemonMetadataProviderTest extends TestCase {
+public class IPokemonMetadataProviderTest  {
 
     //Cette class est chargé pour un index donné de retourner les métadonnées d’une espèce
 
 private  IPokemonMetadataProvider PMPT;
 
+    PokemonMetadata p1;
+    PokemonMetadata p2;
     @Before
     public  void Start(){
 
 
-        PMPT = Mockito.mock(IPokemonMetadataProvider.class);
+            PMPT = Mockito.mock(IPokemonMetadataProvider.class);
+
+
+            // initialisation des pokemons
+            p1 = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+            p2 = new PokemonMetadata(133, "Aquali", 186, 186, 260);
 
 
 
     }
 
+
+
+    @Test
+    public void getPokemonMetadataTest() throws PokedexException {
+
+
+        doReturn(p2).when(PMPT).getPokemonMetadata(133);
+        doReturn(p1).when(PMPT).getPokemonMetadata(0);
+
+        assertThrows(PokedexException.class, () -> PMPT.getPokemonMetadata(-1));
+        assertThrows(PokedexException.class, () -> PMPT.getPokemonMetadata(152));
+
+        assertEquals(p1, PMPT.getPokemonMetadata(0));
+        assertEquals(p2, PMPT.getPokemonMetadata(133));
+
+        Mockito.doThrow(new PokedexException(" ")).when(PMPT).getPokemonMetadata(Mockito.intThat(i -> i < 0 || i > 150));
+
+
+    }
 
 
 
