88a1efd6e13e8b0c85dc4161fb094dded7d58468
uapv2200646@ad.univ-avignon.fr <benfisara2@gmail.com>
2022-05-09 17:13:49 +0200

    commit Amélioration des test

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -16,14 +16,18 @@ public class PokemonFactory implements IPokemonFactory{
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
 
         try {
-            PokemonMetadata pokemonMetadata = iPokemMetaPro.getPokemonMetadata(index);
+
+
+
+            PokemonMetadata pokemonMetadata = new PokemonMetadataProvider().getPokemonMetadata(index);
+
             Random rand = new Random();
             int attack = rand.nextInt(16);
             int def = rand.nextInt(16);
             int stami = rand.nextInt(16);
-            int iv = (int)((attack + def + stami) * 100 / 45);
+            int iv =  (int)((attack + def + stami) * 100 / 45);
 
-            Pokemon pokemon = new Pokemon(index, pokemonMetadata.getName(), attack, def, stami, cp, hp, dust, candy, iv);
+            Pokemon pokemon = new Pokemon(index, pokemonMetadata.getName(), pokemonMetadata.getAttack()+attack, pokemonMetadata.getDefense()+def, pokemonMetadata.getStamina()+stami, cp, hp, dust, candy, iv);
             return pokemon;
         }
         catch (PokedexException e)
