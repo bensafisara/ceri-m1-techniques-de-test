ba6eaf0d433d6c5a668a92819a97ec862b93efd1
uapv2200646@ad.univ-avignon.fr <benfisara2@gmail.com>
2022-05-09 16:57:10 +0200

    commit Amélioration des test IPokemonFactoryTest.java

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -21,7 +21,7 @@ public class PokemonFactory implements IPokemonFactory{
             int attack = rand.nextInt(16);
             int def = rand.nextInt(16);
             int stami = rand.nextInt(16);
-            double iv =  ((attack + def + stami) * 100 / 45);
+            int iv = (int)((attack + def + stami) * 100 / 45);
 
             Pokemon pokemon = new Pokemon(index, pokemonMetadata.getName(), pokemonMetadata.getAttack()+attack, pokemonMetadata.getDefense()+def, pokemonMetadata.getStamina()+stami, cp, hp, dust, candy, iv);
             return pokemon;
